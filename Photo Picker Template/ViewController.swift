b1c7b08b9bb86a37600e2de70069c5605bfa2eb7
Gregorius Albert <galbert.djaja@gmail.com>
2022-10-31 10:53:39 +0700

    Initial Commit

---
+++ Photo Picker Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Picker Template
+//
+//  Created by Gregorius Albert on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
